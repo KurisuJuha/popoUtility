99ffa0162cadeeab38e9b2cba0ebcb96c21dd88a
KYapp <kukiyomenaisapp@gmail.com>
2023-02-25 16:22:03 +0900

    OnMouseDrag

--- Assets/popoUtility/Runtime/PopoBehaviourUnityEvent.cs
+++ Assets/popoUtility/Runtime/PopoBehaviourUnityEvent.cs
@@ -31,5 +31,6 @@ namespace JuhaKurisu.PopoTools.Utility
         protected virtual void OnJointBreak(float breakForce) { }
         protected virtual void OnJointBreak2D(Joint2D brokenJoint) { }
         protected virtual void OnMouseDown() { }
+        protected virtual void OnMouseDrag() { }
     }
 }
