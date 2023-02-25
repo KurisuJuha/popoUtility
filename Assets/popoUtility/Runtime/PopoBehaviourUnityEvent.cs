d097cc9de34fb2576ea30f6478e3218e779b23eb
KYapp <kukiyomenaisapp@gmail.com>
2023-02-25 16:24:29 +0900

    OnRenderObject

--- Assets/popoUtility/Runtime/PopoBehaviourUnityEvent.cs
+++ Assets/popoUtility/Runtime/PopoBehaviourUnityEvent.cs
@@ -45,5 +45,6 @@ namespace JuhaKurisu.PopoTools.Utility
         protected virtual void OnPreCull() { }
         protected virtual void OnPreRender() { }
         protected virtual void OnRenderImage(RenderTexture src, RenderTexture dest) { }
+        protected virtual void OnRenderObject() { }
     }
 }
