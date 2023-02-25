417e30cc8dd8fe2de27cfcbbedd4005f2efa4adb
KYapp <kukiyomenaisapp@gmail.com>
2023-02-25 16:21:16 +0900

    OnDrawGizmos

--- Assets/popoUtility/Runtime/PopoBehaviourUnityEvent.cs
+++ Assets/popoUtility/Runtime/PopoBehaviourUnityEvent.cs
@@ -24,5 +24,6 @@ namespace JuhaKurisu.PopoTools.Utility
         protected virtual void OnControllerColliderHit(ControllerColliderHit hit) { }
         protected virtual void OnDestroy() { }
         protected virtual void OnDisable() { }
+        protected virtual void OnDrawGizmos() { }
     }
 }
