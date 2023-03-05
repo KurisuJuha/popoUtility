f84cc5253a1ca438e77113ff5556f97a4cb18ddb
KYapp <kukiyomenaisapp@gmail.com>
2023-03-05 20:21:41 +0900

    singleton

---
+++ Assets/popoUtility/Runtime/Singleton.cs
@@ -0,0 +1,48 @@
+using System;
+using UnityEngine;
+
+namespace JuhaKurisu.PopoTools.Utility
+{
+    public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        private static T instance;
+        public static T Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    Type t = typeof(T);
+
+                    instance = (T)FindObjectOfType(t);
+                    if (instance == null)
+                    {
+                        Debug.LogError("singleton error");
+                    }
+                }
+
+                return instance;
+            }
+        }
+
+        virtual protected void Awake()
+        {
+            CheckInstance();
+        }
+
+        protected bool CheckInstance()
+        {
+            if (instance == null)
+            {
+                instance = this as T;
+                return true;
+            }
+            else if (Instance == this)
+            {
+                return true;
+            }
+            Destroy(this);
+            return false;
+        }
+    }
+}
